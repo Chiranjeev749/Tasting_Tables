d597dbae96c5af44dae5f0969349ebb4b67874c3
Chiranjeev <64792076+Chiranjeev749@users.noreply.github.com>
2023-05-31 21:37:18 +0530

    Delete my_site directory

--- my_site/resturant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name = "index"),
-    path('home/', views.home, name='home'),
-    path('book-table/<int:restaurant_id>/', views.book_table, name='book_table'),
-    path('view-menu/<int:restaurant_id>/', views.view_menu, name='view_menu'),
-]
